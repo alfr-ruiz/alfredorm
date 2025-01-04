01f75a50f7e15132a18f0f06ebbe2ae30851bcdc
alfr-ruiz <alfredoruizmalca@gmail.com>
2025-01-04 12:42:17 -0500

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  content: ["./src/**/*.html"],
+  theme: {
+    extend: {
+      colors: {
+        navbarBg: "#212121",
+        navbarItem: "#B4B4B4",
+        navbarItemHover: "#ECECEC",
+      },
+      fontFamily: {
+        sans: ["Helvetica", "Arial", "sans-serif"],
+      },
+    },
+  },
+  plugins: [],
+};
